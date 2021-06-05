b7d1bb50f9b4618cbc5b1117e814df6907feeff2
root <root@luchy.hacettepe.edu.tr>
2021-06-05 13:14:59 +0430

    puzzle project. Done with cat and dogs, rotate ability included! Created on Windows

---
+++ Assets/TouchRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+
+    // Update is called once per frame
+    private void OnMouseDown()
+    {
+        if(!GameControl.youWin)
+            transform.Rotate(0f,0f,90f);
+    }
+}
